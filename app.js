428eafaaa8f29ad6271086b47197ad79c0b4f3fd
Zetos Lima <cesar_manoel@hotmail.com>
2019-09-07 20:26:58 -0300

    Express start

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const room = require('./routes/room');
+const chat = require('./routes/chat');
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/rooms', express.static(path.join(__dirname, 'dist')));
+app.use('/api/room', room);
+app.use('/api/chat', chat);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
